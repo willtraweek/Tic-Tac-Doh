cd75d58be6f99e667da5a785edf263431383e55e
willtraweek <will.traweek@gmail.com>
2023-07-05 16:36:02 -0400

    remove unnecessary vars

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -2,8 +2,6 @@
 public class GameData : MonoBehaviour
 {
     public static GameData Instance { get; private set; }
-    public bool IsPlayerTurn { get; set; } = false;
-    public bool MostRecentTurnWasPlayer { get; set; } = false;
     public bool DidPlayerWin { get; set; } = false;
     public float TimeOfLastAnswer { get; set; } = 0.0f;
     public bool IsTriviaTime { get; set; } = false;
